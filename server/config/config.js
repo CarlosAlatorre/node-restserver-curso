ec29551754509b9e3daf0c0074e9a60f45cca477
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2018-08-11 13:24:20 -0700

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://cafe-carlos:HAlfiakGabWeaw8@ds219432.mlab.com:19432/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
